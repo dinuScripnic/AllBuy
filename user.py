4355e7f3a4e8b06a1457b9668d3ce616d6a318d6
dinuScripnic <scripnicdinu2003@gmail.com>
2022-05-01 18:27:47 +0200

    Added log_in, register, add_product, get user from database

--- user.py
+++ user.py
@@ -1,15 +1,17 @@
 import datetime
+import uuid
 
 
 class User:
 
-    def __init__(self, _id, name, email, password):
-        self.id = _id
+    def __init__(self, name, email, password):
+        self.id = str(uuid.uuid1())
         self.name = name
         self.email = email
         self.password = password
-        self.join_date = datetime.now()
+        self.join_date = datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")
         self.basket = None
+        self.selling = None
 
     def assign_basket(self, basket):
         self.basket = basket
